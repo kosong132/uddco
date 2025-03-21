729f5fc9ed2a9c7d0064d2cf9cf85cdc142478c3
kosong132 <yongsiong@graduate.utm.my>
2025-03-22 00:33:51 +0800

    ks fyp

---
+++ src/test/java/com/uddco/UddCoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uddco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UddCoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
